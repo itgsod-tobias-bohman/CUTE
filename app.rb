fd3db7594d7562e22bcf891354826fb9d100fbfc
itgsod-nali-sapan <nali.sapan@Stahp.local>
2015-02-18 14:19:31 +0100

    login and out feature added Fixes #3, Fixes #4
    /tobias

--- app.rb
+++ app.rb
@@ -4,22 +4,36 @@ class App < Sinatra::Base
   require 'omniauth/strategies/steam'
 
   puts 'STEAM API KEY'               # Write your Steam API Key in field
-  api_key = 'AC9AA39CE23FF24F3A545A0229CF5244' # Key can be found in Readme
+  api_key = 'AC9AA39CE23FF24F3A545A0229CF5244'
 
-  use Rack::Session::Cookie
+  enable :sessions
 
   use OmniAuth::Builder do
     provider :steam, api_key, :storage => OpenID::Store::Filesystem.new('/tmp')
   end
 
+  get '/debug' do
+    session.inspect
+  end
+
   get '/' do
+    if session[:loggedInSteamUser]
+      @log = true
+    else
+      @log = false
+    end
     slim :index
+  end
+
+  get '/logout' do
+    session.clear
+    redirect back
   end
 
   post '/auth/steam/callback' do
     @data = request.env['omniauth.auth']
     @id = @data['uid'].to_s
-    @steam_name = @data['info']['nickname'].to_s
+    session[:loggedInSteamUser] = @data['info']['nickname'].to_s
 
     @steamuser = SteamUser.first_or_create(steam_64: @id)
     redirect back
